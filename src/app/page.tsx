f674df26f1e08d465f0695d8ccae8fb5e2ada55c
Hayley <hlzulkoski@gmail.com>
2026-06-08 12:20:56 -0600

    basic home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,21 @@
+import AppNavBar from "@/app/components/AppNavBar";
 import Image from "next/image";
-import styles from "./page.module.css";
+import homeScreenHero from "@/app/images/home-screen-hero.jpg";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 export default function Home() {
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div>
+      <AppNavBar />
+      <Box>
+        <Image src={homeScreenHero} alt="Home Image" style={{ aspectRatio: '3/2', width: '100%', height: '100%' }} />
+        <Box sx={{ position: 'absolute', top: 50, backgroundColor: 'rgba(15, 126, 145, 0.5)', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+          <Typography sx={{ color: '#ffffff', textTransform: 'uppercase', fontWeight: 700, padding: 8 }} variant="h2">Hannah Kimball</Typography>
+          <Typography sx={{ color: '#ffffff', textTransform: 'uppercase', fontWeight: 600, padding: 8, paddingTop: 0 }} variant="h5">Composer - Educator </Typography>
+        </Box>
+      </Box>
     </div>
   );
 }
